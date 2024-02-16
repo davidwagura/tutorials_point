f9f14bb6f9ed63b112a51136355b59ce6eae8493
David wanjohi <davidwagura83@gmail.com>
2024-02-16 12:17:28 +0300

    app config and multiple application instances

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import { createApp } from 'vue'
 
 import App from './App.vue'
+app.config.console.errorHandler = (err) =>{
+
+}
 
 const app = createApp
 ({
@@ -10,4 +13,17 @@ const app = createApp
         }
     }
 })
+const app1 = createApp({
+
+})
+
+app1.mount('#container-1')
+
+const app2 = createApp({
+
+})
+
+app2.mount('#container-2')
+
 app.mount('#app')
+app.component('TodoDeleteButton', TodoDeleteButton)
